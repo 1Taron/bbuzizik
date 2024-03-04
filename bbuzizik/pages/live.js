e90802075e7beb93d347a0b8435bbc22f706537b
Jang URam <90294651+Ukdam@users.noreply.github.com>
2024-03-04 21:07:33 +0900

    라이브 화면 2차
    채팅 기본 완성, 메인 레이아웃

--- bbuzizik/pages/live.js
+++ bbuzizik/pages/live.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Sidebar from '../components/sidebar';
 import Header from '../components/Header';
-import styles from '../css/main.module.css';
 import livestyles from '../css/main_live.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faFaceSmile } from '@fortawesome/free-regular-svg-icons';
+import Image from 'next/image';
+import shit from '../public/shit01.png';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 export default function Live() {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -21,9 +23,18 @@ export default function Live() {
     <>
       <Sidebar isExpanded={isExpanded} onToggle={() => setIsExpanded(!isExpanded)} />
       <Header />
-      <div style={{ paddingLeft: isExpanded ? 240 : 66 }}>
-        <div className={livestyles.homecontainer}>
-          <h1>라이브</h1>
+      <div style={{ marginLeft: isExpanded ? 240 : 66 }}>
+        <div className={livestyles.homecontainer} style={{ width: isExpanded ? 'calc(100vw - 240px - 350px)' : 'calc(100vw - 66px - 350px)' }}>
+          <div className={livestyles.livecontainer}>라이브 화면</div>
+          <div className={livestyles.infocontainer}>
+            <div className={livestyles.infobox}>
+              <p>방 제목</p>
+              <div className={livestyles.infobox_content}>
+                <div className={livestyles.infobox_content_profile}></div>
+                <div className={livestyles.infobox_content_remain}>이름 카테고리 등등</div>
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* 채팅 */}
@@ -36,16 +47,29 @@ export default function Live() {
           {/* 입력창 */}
           <div className={livestyles.main_live_chat_inputbox_layout}>
                 <div className={livestyles.main_live_chat_inputbox_wrapper}>
+                    <div className={livestyles.main_live_chat_inputbox_badge}></div>
                     <FontAwesomeIcon
-                    icon={faSearch}
+                    icon={faFaceSmile}
                     style={{
                         position: "absolute",
-                        top: "15px",
-                        left: "10px",
+                        top: "14px",
+                        right: "10px",
                         color: "black",
                         fontSize: "20px",
+                        cursor:"pointer",
                     }}
                     />
+                    {/* nextjs이미지 */}
+                    <Image src={shit} 
+                    style={{
+                          position: "absolute",
+                          top: "14px",
+                          right: "40px",
+                          width: "20px", 
+                          height: "20px",
+                          cursor:"pointer",
+                      }} />
+                    
                     <input
                     type="text"
                     className={
@@ -53,14 +77,19 @@ export default function Live() {
                     }
                     value={chatText}
                     onChange={chat_handleChange}
-                    style={{ paddingLeft: "40px" }}
+                    placeholder='채팅을 입력해주세요.'
+                    style={{ paddingLeft: "40px", fontSize: "14px", backgroundColor: "#63666A" }}
                     />
-                    <input
+                    <button
                     type="button"
                     className={livestyles.main_live_chat_inputbox_btn}
                     onClick={chat_handleClick}
-                    value="입력"
+                    >
+                    <FontAwesomeIcon 
+                    icon={faPaperPlane} 
+                    style={{ color: '#E3E3E3', width:"20px", height:"20px", marginRight:"3px" }} 
                     />
+                    </button>
                 </div>
                 
               </div>
