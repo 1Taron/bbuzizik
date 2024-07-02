07de3a6c511ca39763caa597f60f04c37bde646b
1Taron <she11466@naver.com>
2024-07-02 19:24:37 +0900

    카테고리 전체보기 완

--- bbuzizik/src/pages/Category_all.js
+++ bbuzizik/src/pages/Category_all.js
@@ -29,6 +29,16 @@ export default function Category_all() {
         fetchCategories();
     }, [db]);
 
+    const formatViewers = viewers => {
+        const numViewers = Number(viewers);
+        if (numViewers >= 10000) {
+            return `${(numViewers / 10000).toFixed(1)}만명`;
+        } else {
+            // 1000자리 단위마다 쉼표를 찍어서 반환
+            return `${numViewers.toLocaleString()}명`;
+        }
+    };
+
     return (
         <div className={styles.background}>
             <Sidebar isExpanded={isExpanded} onToggle={() => setIsExpanded(!isExpanded)} />
@@ -45,7 +55,8 @@ export default function Category_all() {
                                 <div className={styles.Category_list_Thumnail}>
                                     <img src={category.img} alt={category.name} />
                                 </div>
-                                <div>{category.name}</div>
+                                <div className={styles.Category_name}>{category.name}</div>
+                                <div className={styles.Category_viewer}>시청자 {formatViewers(Number(category.total_viewer))}</div>
                             </a>
                         </li>
                     ))}
